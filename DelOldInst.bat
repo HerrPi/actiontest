4e5963caa62bb6ca93af8f3a8842e1e0d546321c
Simon Brenner <s.brenner@mbaec.de>
2023-07-14 11:03:37 +0200

    v8 artifact

--- DelOldInst.bat
+++ DelOldInst.bat
@@ -1,8 +1,8 @@
-XCOPY "C:\xampp\htdocs\audio\private.php" "C:\xampp\htdocs\" /S /E /C /H /R /Y
-DEL /F/Q/S "C:\xampp\htdocs\audio\" > NUL
-RMDIR /Q/S "C:\xampp\htdocs\audio\"
-XCOPY *.* "C:\xampp\htdocs\audio\" /S /E /C /H /R /Y
-MOVE "C:\xampp\htdocs\private.php" "C:\xampp\htdocs\audio\"
+rem XCOPY "C:\xampp\htdocs\audio\private.php" "C:\xampp\htdocs\" /S /E /C /H /R /Y
+DEL /F/Q/S "C:\xampp\htdocs\simactiontest\" > NUL
+RMDIR /Q/S "C:\xampp\htdocs\simactiontest\"
+XCOPY *.* "C:\xampp\htdocs\simactiontest\" /S /E /C /H /R /Y
+rem MOVE "C:\xampp\htdocs\private.php" "C:\xampp\htdocs\audio\"
 SET FFPath="%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe"
 IF NOT DEFINED ProgramFiles(x86) SET FFPath="%ProgramFiles%\Mozilla Firefox\firefox.exe"
-%FFPath% "localhost/audio/db_upgrade.php"
+rem %FFPath% "localhost/audio/db_upgrade.php"
